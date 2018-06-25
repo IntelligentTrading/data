1305f514ff4afb9d1fcccb753f44d30cfdf1f472
Alex Borey <lexnex@gmail.com>
2018-06-25 14:43:55 +0200

    remove all links to HistoryPrice

--- apps/channel/management/commands/fetch_tickers.py
+++ apps/channel/management/commands/fetch_tickers.py
@@ -9,14 +9,11 @@ import schedule
 import ccxt
 
 from django.core.management.base import BaseCommand
-#from requests import get, RequestException
 
 from apps.channel.models import ExchangeData
-from apps.channel.models import HistoryPrice
 
 from apps.channel.tickers import Tickers, get_usdt_rates_for, to_satoshi_int
 from apps.channel.pubsub_queue import publish_message_to_queue
-#from apps.common.utilities.multithreading import start_new_thread
 
 from settings import EXCHANGE_MARKETS, AWS_SNS_TOPIC_ARN
 from settings import TICKERS_MINIMUM_USD_VOLUME
@@ -67,9 +64,6 @@ def fetch_and_process_one(exchange, usdt_rates):
     # send message with prices/volumes to Core app
     publish_message_to_queue(message=json.dumps(tickers.symbols_info), topic_arn=AWS_SNS_TOPIC_ARN, subject='prices_volumes')
 
-    # store price/volume data into HistoryPrice model
-    save_to_history(tickers.tickers, exchange)
-
 
 def save_to_db(tickers, exchange):
     timestamp = tickers[list(tickers)[0]]['timestamp']/1000 # convert from timestamp in miliseconds
@@ -79,32 +73,3 @@ def save_to_db(tickers, exchange):
         timestamp=timestamp,
     )
     logger.debug(f">>> Saved to db: {exchange}")
-
-
-def save_to_history(tickers, exchange):
-    # tickers: {'BCN/BTC': {'symbol': 'BCN/BTC', 'timestamp': 1523439353595, 'datetime': '2018-04-11T09:35:54.595Z', 'high': 3.8e-07, 'low': 3.2e-07, 'bid': 3.4e-07, 'bidVolume': None, 'ask': 3.5e-07, 'askVolume': None, 'vwap': None, 'open': 3.1999999999999995e-07, 'close': 3.4e-07, 'last': 3.4e-07, 'previousClose': None, 'change': 2.000000000000002e-08, 'percentage': 6.25, 'average': 3.2999999999999996e-07, 'baseVolume': 146970152.54629916, 'quoteVolume': 51.17040999, 'info': {'id': 7, 'last': '0.00000034', 'lowestAsk': '0.00000035', 'highestBid': '0.00000034', 'percentChange': '0.06250000', 'baseVolume': '51.17040999', 'quoteVolume': '146970152.54629916', 'isFrozen': '0', 'high24hr': '0.00000038', 'low24hr': '0.00000032'}},
-
-    source_code = next((code for code, source_text in SOURCE_CHOICES if source_text==exchange.lower()), None)
-
-    for trading_pair, value in tickers.items():
-        (transaction_currency, counter_curency_text) = trading_pair.split('/')
-        if counter_curency_text in COUNTER_CURRENCIES:
-            counter_currency_code = next((code for code, counter_currency in COUNTER_CURRENCY_CHOICES if counter_currency==counter_curency_text), None)
-        else:
-            counter_currency_code = None
-        if counter_currency_code is None:
-            continue # skip this counter currency
-        
-        HistoryPrice.objects.create(
-            timestamp = value['timestamp']/1000, # convert from timestamp in miliseconds UTC
-            source = source_code,
-            transaction_currency = transaction_currency[0:8],
-            counter_currency = counter_currency_code,
-            open_price = to_satoshi_int(value['open']),
-            high_price = to_satoshi_int(value['high']),
-            low_price = to_satoshi_int(value['low']),
-            close_price = to_satoshi_int(value['close']),
-            base_volume = value['baseVolume'],
-        )
-
-    logger.debug(f">>> History saved to db for exchange: {exchange}")
--- apps/channel/models/__init__.py
+++ apps/channel/models/__init__.py
@@ -1,9 +1,7 @@
 from apps.channel.models.exchange_data import ExchangeData
-from apps.channel.models.history_price import HistoryPrice
 from apps.channel.models.blockchain_stats import BlockchainStats
 
 __all__ = [
     "ExchangeData",
-    "HistoryPrice",
     "BlockchainStats",
 ]
