f6b9dc408b74e010589287056a23fcee1af2d503
Tom Counsell <other@tomcounsell.com>
2018-08-18 12:54:51 +0200

    python formatting

--- apps/channel/management/commands/firehose_ohlc_data.py
+++ apps/channel/management/commands/firehose_ohlc_data.py
@@ -10,7 +10,8 @@ from settings import ITF_API, ITF_API_KEY
 
 logger = logging.getLogger(__name__)
 
-class Command(BaseCommand): # firehose_ohlc_data
+
+class Command(BaseCommand):  # firehose_ohlc_data
     help = "=== THE FIREHOSE ==="
 
     def handle(self, *args, **options):
@@ -18,29 +19,29 @@ class Command(BaseCommand): # firehose_ohlc_data
 
         timestamp = jan_1_2017 = 1483228800
         today = int(time.time())
-        increment = 86400 # seconds in 1 day
+        increment = 86400  # seconds in 1 day
 
         while timestamp < today:
             timestamp += increment
             logger.info("Running ... " + datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
 
             for exchange_data_object in ExchangeData.objects.order_by('timestamp').filter(
-                    timestamp__gt=(timestamp-increment), timestamp__lte=timestamp
+                    timestamp__gt=(timestamp - increment), timestamp__lte=timestamp
             ):
                 tickers_object = Tickers(exchange=exchange_data_object.source)
                 tickers_object.tickers = exchange_data_object.data
 
                 for symbol, symbol_info in tickers_object.tickers.items():
 
-                    if not symbol.count('/') == 1: # check format is like "ETH/BTC"
+                    if not symbol.count('/') == 1:  # check format is like "ETH/BTC"
                         # logger.debug(f'Skipping symbol: {symbol}')
-                        continue # skip malformed currency pairs
+                        continue  # skip malformed currency pairs
 
                     if not tickers_object._symbol_allowed(symbol_info=symbol_info):
                         # logger.debug(f'Skipping symbol: {symbol}')
                         continue
 
-                    ticker = symbol.replace("/","_")
+                    ticker = symbol.replace("/", "_")
                     headers = {'API-KEY': ITF_API_KEY}
                     r = requests.put(f'{ITF_API}/v3/historical_data/{ticker}', headers=headers,
                                      json={
